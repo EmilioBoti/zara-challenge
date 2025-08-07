1c886d267977f7ce3ddceb50b522d444c4f9c54a
Emilio Botier <emiliobotier@gmail.com>
2025-08-07 16:11:07 +0200

    feat: added animation to product list

--- src/components/mobileCard/ListProduct.tsx
+++ src/components/mobileCard/ListProduct.tsx
@@ -2,6 +2,7 @@ import MobileCard from '../mobileCard/MobileCard'
 import styles from './ListProduct.module.css'
 
 import { Product } from '../../models/appModels/Product'
+import { motion } from 'framer-motion'
 
 
 type ListProductProps = {
@@ -13,12 +14,16 @@ export default function ListProduct({ items, onClickEvent }: ListProductProps) {
   return (
     <ul className={styles.productContainer}>
       {items.map((product, index) => (
-        <li key={index}>
+        <motion.li key={index}
+          initial={{ opacity: 0, x: -2 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.4, delay: index * 0.1 }}
+        >
           <MobileCard 
             product={product}
             onClickEvent={ (product: Product) => onClickEvent(product) }
           />
-        </li>
+        </motion.li>
       ))}
     </ul>
   )
--- src/components/search/SearchBar.tsx
+++ src/components/search/SearchBar.tsx
@@ -23,7 +23,7 @@ export default function SearchBar(
   useEffect(() => {
     let handler = setTimeout(() => {
       setDebouncedValue(value)
-    }, 500)
+    }, 200)
 
     return () => {
       clearTimeout(handler)
@@ -37,6 +37,9 @@ export default function SearchBar(
   return (
     <motion.div
       className={styles.searchContainer}
+      initial={{ opacity: 0, y: -5 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4 }}
     >
       <div className={styles.searchInputContainer}>
         <input
